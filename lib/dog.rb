98c79a739ac4a2b3860f3aff5fb017280a79217f
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-19 11:28:23 +0300

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
 
+    # set
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+
+    # get
+    def name
+        @this_dogs_name
+    end
+
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+puts lassie.name 
